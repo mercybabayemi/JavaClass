fd2de500163249ac5098f0f708fa6ef80e957780
mercybabayemi <mercyjanet013@gmail.com>
2024-10-25 07:12:50 +0100

    add

---
+++ java/Class Task Jennifer/PrintDate.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+public class PrintDate{
+	public static void main(String[ ]args){
+
+Date now = new Date();
+System.out.print("Current Date time: " + now);
+}
+}
---
+++ java/Class Task Jennifer/PrintResult.java
@@ -0,0 +1,6 @@
+public class PrintResult{
+	public static void main(String[]args){
+	int sum = 2 + 2 + 3;
+	System.out.printf("The sum is %d",sum);
+}
+}
