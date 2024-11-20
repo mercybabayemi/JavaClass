864b2e231be40addf8fbfd7539dbb94cb4da5ab1
mercybabayemi <mercyjanet013@gmail.com>
2024-11-20 12:09:21 +0100

    add

---
+++ java/Class Task Jennifer/SquareResult.java
@@ -0,0 +1,15 @@
+//grouptask4
+
+public class SquareResult{
+	public static void main(String[] args){
+
+	int number = 1;
+
+	while( number <= 5){
+		
+		int square = number * number;
+		System.out.print(square + " ");
+		 number++;
+	}
+}
+}
