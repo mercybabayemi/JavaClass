285214d4f473335a4b0add0b55430c4b19cb0c93
mercybabayemi <mercyjanet013@gmail.com>
2024-12-03 13:00:12 +0100

    update

--- java/Java/PhaseGateOne/Lagbaja School/LagbajaSchoolScoreGrading.java
+++ java/Java/PhaseGateOne/Lagbaja School/LagbajaSchoolScoreGrading.java
@@ -7,6 +7,10 @@ public class LagbajaSchoolScoreGrading{
 		String teacher = input.nextLine();
 
 		int students = getStudentNumber(input, teacher);
+		int subjects = getSubjects(input, teacher);
+		
+		int[][] studentGrade = new int[students][subjects];
+		getScore(input, students, subjects, studentGrade);
 	}
 
 	public static int getStudentNumber(Scanner input, String teacher){
@@ -14,4 +18,22 @@ public class LagbajaSchoolScoreGrading{
 		int numberOfStudents = input.nextInt();
 		return numberOfStudents;
 	}
+	public static int getSubjects(Scanner input, String teacher){
+		System.out.printf("Mr/Mrs/Miss %s, how many subjects do you want to grade the students on? %n",teacher);
+		int studentSubjects = input.nextInt();
+		return studentSubjects;
+	}
+	public static void getScore(Scanner input, int students,int subjects, int[][] studentGrade){
+		for(int row = 0; row < students; row++){
+			for(int column = 0; column < subjects; column++){
+				System.out.println("Enter a number between 1 - 100 as grade for subject["+ row + 1 +"], Student["+ column + 1 +"]:");
+				studentGrade[row][column] = input.nextInt();
+				while(studentGrade[row][column] < 0 || studentGrade[row][column] > 100){
+					System.out.println("Invalid input!!!\nEnter a valid input: ");
+					studentGrade[row][column] = input.nextInt();
+				}
+			}
+		}
+	}
+	
 }
