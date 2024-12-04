9b5022863d588c4962831c93deef2728400630a4
mercybabayemi <mercyjanet013@gmail.com>
2024-12-04 05:27:59 +0100

    update

--- java/Java/PhaseGateOne/Lagbaja School/LagbajaSchoolScoreGrading.java
+++ java/Java/PhaseGateOne/Lagbaja School/LagbajaSchoolScoreGrading.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class LagbajaSchoolScoreGrading{
 	public static void main(String[] args){
 		System.out.println("LAGBAJA GROUP OF SCHOOL, LAGOS STATE.\nWELCOME TO THE OFFICIAL GRADING WEBSITE");
@@ -12,28 +13,33 @@ public class LagbajaSchoolScoreGrading{
 		int[][] studentGrade = new int[students][subjects];
 		getScore(input, students, subjects, studentGrade);
 		displayTableHeader(subjects);
-		displayTableContent(studentGrade);
+		displayTableContent(studentGrade, subjects, students);
+		System.out.println("SUBJECT SUMMARY");
+		getSubjectCalculated(studentGrade, subjects, students);
 	}
 
 	public static int getStudentNumber(Scanner input, String teacher){
-		System.out.printf("Mr/Mrs/Miss %s, how many students do you want to grade? %n",teacher);
+		System.out.printf("Mr/Mrs/Miss %s, how many students do you have? %n",teacher);
 		int numberOfStudents = input.nextInt();
+		System.out.println("Saving>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\nSaved successfully.");
 		return numberOfStudents;
 	}
 	public static int getSubjects(Scanner input, String teacher){
 		System.out.printf("Mr/Mrs/Miss %s, how many subjects do you want to grade the students on? %n",teacher);
 		int studentSubjects = input.nextInt();
+		System.out.println("Saving>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\nSaved successfully.");
 		return studentSubjects;
 	}
 	public static void getScore(Scanner input, int students,int subjects, int[][] studentGrade){
 		for(int row = 0; row < students; row++){
 			for(int column = 0; column < subjects; column++){
-				System.out.println("Enter a number between 1 - 100 as grade for subject ("+ (row + 1) +"), Student("+ (column + 1) +"):");
+				System.out.println("Enter a number between 1 - 100 as score for subject ("+ (row + 1) +"), Student("+ (column + 1) +"):");
 				studentGrade[row][column] = input.nextInt();
 				while(studentGrade[row][column] < 0 || studentGrade[row][column] > 100){
 					System.out.println("Invalid input!!!\nEnter a valid input: ");
 					studentGrade[row][column] = input.nextInt();
 				}
+				System.out.println("Saving>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\nSaved successfully.");
 			}
 		}
 	}
@@ -46,14 +52,59 @@ public class LagbajaSchoolScoreGrading{
 		System.out.print("TOT \tAVE \tPOS ");
 		System.out.println("\n==========================================================================================");
 	}
-	public static void displayTableContent(int[][] studentGrade){
-		for (int column = 0; column < studentGrade[0].length; column++) {
-			System.out.print("Student (" + (column + 1) + ")  \t");
-        			for (int row = 0; row < studentGrade.length; row++) {
+	public static void displayTableContent(int[][] studentGrade, int subjects, int students){
+		for (int row = 0; row < students; row++) {
+			System.out.print("Student (" + (row + 1) + ")  \t");
+        			for (int column = 0; column < subjects; column++) {
 					System.out.print(studentGrade[row][column] + "\t");
 				}
 		System.out.println();
    		}			System.out.println("\n==========================================================================================");
 
 	}
+
+
+	public static void getSubjectCalculated(int[][] studentGrade, int subjects,int students) {
+   		int subjectTotal = 0;
+		int averageSubjectScore = 0;
+    		int lowestScore = Integer.MAX_VALUE;
+    		String lowestIndex = "";
+		int highestScore = -1;
+    		String highestIndex = "";
+		int passMark = 50;
+    		int passCount = 0;
+	  	int failCount = 0;
+
+    		for (int column = 0; column < subjects; column++) {
+        	System.out.print("Subject (" + (column + 1) + ") \t");
+        		for (int row = 0; row < studentGrade.length; row++) {
+            			subjectTotal += studentGrade[row][column];
+				if (studentGrade[row][column] > highestScore) {
+               				highestScore = studentGrade[row][column];
+                			highestIndex = "Student (" + (row + 1) + ")";
+				}
+				if (studentGrade[row][column] < lowestScore) {
+                			lowestScore = studentGrade[row][column];
+                			lowestIndex = "Student (" + (row + 1) + ")";
+				}
+				if (studentGrade[row][column] >= passMark) {
+               				passCount++;
+           			} else {
+               			 	failCount++;
+          			 }
+
+        		}
+        		System.out.println();
+			System.out.printf("Highest scoring student in Subject (%d) is: %s scoring %d\n", (column + 1), highestIndex, highestScore);
+			System.out.printf("Lowest scoring student in Subject (%d) is: %s scoring %d\n", (column + 1), lowestIndex, lowestScore);
+			System.out.printf("Total scores in Subject (%d) is: %d\n", (column + 1), subjectTotal);
+			averageSubjectScore = subjectTotal / students;
+			System.out.printf("Average score for Subject (%d) is: %d\n", (column + 1), averageSubjectScore);
+			System.out.printf("Pass count for Subject (%d) is: %d\n", (column + 1), passCount);
+        		System.out.printf("Fail count for Subject (%d) is: %d\n", (column + 1), failCount);
+
+    		}
+	}
+
+    		
 }
