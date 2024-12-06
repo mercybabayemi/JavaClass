1ffc81776dd54d8dae5f00a49c0cb6fbde3170c8
mercybabayemi <mercyjanet013@gmail.com>
2024-12-06 02:57:40 +0100

    update

---
+++ java/Java/PhaseGateOne/TestingFormatter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class TestingFormatter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter last start date (yyyy-MM-dd): ");
+        String lastStartDateStr = input.nextLine();
+        
+        DateTimeFormatter date = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate lastStartDate = LocalDate.parse(lastStartDateStr, date);
+        
+        System.out.print("The date is : " + lastStartDate);
+    }
+}
