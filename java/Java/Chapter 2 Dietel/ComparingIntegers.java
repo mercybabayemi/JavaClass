d1baf169897396bebf583f55c8b88df1aff46c61
mercybabayemi <mercyjanet013@gmail.com>
2024-12-10 02:05:12 +0100

    complete chapter 2 exercise

---
+++ java/Java/Chapter 2 Dietel/ComparingIntegers.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class ComparingIntegers {
+	public static void main(String[] args) {
+
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter first integer: ");
+	int x = input.nextInt();
+
+	int square = x * x;
+
+	if(x > 100 && square > 100) {
+		System.out.println("Number and its square is greater than 100");
+	}
+
+	if(x == 100 && square == 100) {
+		System.out.println("Number and its square is equal to 100");
+	}
+
+	if(x != 100 && square != 100) {
+		System.out.println("Number and its square is not equal to 100");
+	}
+
+	if(x < 100 && square < 100) {
+		System.out.println("Number and its square is less than 100");
+	}
+
+	}
+}
+
---
+++ java/Java/Chapter 2 Dietel/Product.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Product {
+	public static void main(String[] args) {
+	int numberOne = 2;
+	int numberTwo = 5;
+	int numberThree = 6;
+
+	int product = numberOne * numberTwo * numberThree;
+	System.out.println("Product is " + product);
+
+
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter first integer: ");
+	int x = input.nextInt();
+
+	System.out.println("Enter second integer: ");
+	int y = input.nextInt();
+
+
+	System.out.println("Enter third integer: ");
+	int z = input.nextInt();
+
+	int result = x * y * z;
+	System.out.println("Result is " + result);
+
+	int p = 5;
+	System.out.printf("%d", p + 2 * 4);
+	System.out.printf("%d", p * 2 + 4);
+
+	}
+
+}
